ec3956e17b26a0928795379eaa9fc3d4ac9c6306
tt-bb <ttbb_@outlook.com>
2022-04-30 16:11:40 +0200

    Add IP infos (city, country, ISP)

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import speedtest
-from requests import get
+import requests
+import json
+from env import API_KEY
 
-
-# Get your IP address
-ip = get('https://api.ipify.org').text
 
 print("~~~ SPEEDTEST ~~~")
 print("in progress...")
-print(f"From your IP address : {ip}")
+
+# Get IP information
+ip = requests.get('https://api.ipify.org').text
+key = API_KEY
+url = f"https://ipgeolocation.abstractapi.com/v1/?api_key={key}&ip_address={ip}"
+response = requests.get(url)
+result = json.loads(response.content)
+
+print(f"\tFrom your IP address : {ip}")
+city = result["city"]
+print(f"\tCity : {city}")
+country = result["country"]
+print(f"\tCountry : {country}")
+fai = result["connection"]["isp_name"]
+print(f"\tFAI : {fai}")
+
+# Speedtest
 test = speedtest.Speedtest()
 
 # Speeds in bytes
@@ -20,5 +35,5 @@ upload *= (9.537 * (10 ** -7))
 download = round(download, 2)
 upload = round(upload, 2)
 
-print(f"\tDownload = {download} Mbps")
-print(f"\tUpload = {upload} Mbps")
+print(f"\n\t∇ : {download} Mbps")
+print(f"\t∆ : {upload} Mbps")
